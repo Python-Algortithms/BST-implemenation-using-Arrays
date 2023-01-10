5d810fdf00c95ded4867090a4b3565084aa600c3
ibrahim Ali Kaldesh <66179574+Ibrahim-Kaldesh@users.noreply.github.com>
2023-01-10 06:23:23 +0200

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -9,5 +9,17 @@ def BSUM(a , l , h):
     return a[m] + BSUM(a,l,m) + BSUM(a,m+1,h)
 
 
-a = [1,5,-1,2,4]
-print(BSUM(a,0,len(a)))
+# The first ele >= target
+def upper_bound (a,low,high,x):
+ # 0 0 0 0 0 1 1 1 1 1 1 1 1
+ if low >= high :
+  return a[high]
+ m = (low+high)//2
+ if a[m] >= x :high = m
+ else:low = m + 1
+ return upper_bound (a,low,high,x)
+
+if __name__ = '__main__':
+ a = [1,3,5,9,11,22,60,80,100]
+ ans = upper_bound(a,0,len(a)-1,x = 2)
+ print(ans)
